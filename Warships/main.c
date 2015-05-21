bb3a75bdbe60f5b69d39ab043f1446b4c7fc45d9
Natalia Zagan <cruelnat@Cruelnat.local>
2015-05-21 20:08:10 +0200

    Initial Commit

---
+++ Warships/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Warships
+//
+//  Created by Natalia Zagan on 21/05/2015.
+//  Copyright (c) 2015 Natalia Zagan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
